03c2c197dd49e2f5df7baed11889960f131547cf
davidmarten <37086475+davidmarten@users.noreply.github.com>
2023-08-28 17:19:58 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,8 +51,6 @@ numfig = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-#html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = ['_themes']
 html_logo = 'images/q_logo_black.png'
